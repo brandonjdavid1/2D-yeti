a79357beb093136d1eccb5e1f9e3647d715cab52
brandonjdavid1 <57733061+brandonjdavid1@users.noreply.github.com>
2019-11-13 18:21:46 -0600

    Player controls
    Added player controls to yet (Space to jump, A & D to run)

--- Assets/scripts/Platformer_PlayerController.cs
+++ Assets/scripts/Platformer_PlayerController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Platformer_PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private float moveSpeed;
+
+    [SerializeField]
+    private float jumpHeight;
+
+    private Rigidbody2D playerRigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        playerRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.getButtonDown) (KeyCode.Space);
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jumpHeight);
+        }
+        //run right
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerRigidBody.velocity = new Vector2(moveSpeed, playerRigidBody.velocity.y);
+      
+        }
+        //Run left
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerRigidBody.velocity = new Vector2(-1 * moveSpeed, playerRigidBody.velocity.y);
+
+        }
     }
+
+
 }
